e808d5078e4273d5caeb532e1e47037745ce73ad
d-yosh <daicedaice87@gmail.com>
2020-01-16 16:03:18 +0900

    String を CamelCase に変換するための暗黙の型変換を実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,15 @@
 object Main {
+  implicit class CamelString(val src: String) {
+    def toCamelCase: String = {
+        src.split(" ").foldLeft("") {
+          (b, a) => {
+            b + a.head.toUpper + a.tail
+          }
+        }
+    }
+  }
 
+  def main(args: Array[String]): Unit = {
+    println("my name is taro".toCamelCase)
+  }
 }
